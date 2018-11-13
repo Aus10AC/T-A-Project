2d4be9592f94efe950976f39ed10395f0a198223
Private <36454345+Privateth@users.noreply.github.com>
2018-11-13 08:20:03 -0600

    Added Sweetalert + unsolved bug

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-function myFunction() {
+ function myFunction() {
   var x = document.getElementById("myDemo");
   if (x.style.display === "block") {
       x.style.display = "none";
@@ -6,3 +6,11 @@ function myFunction() {
       x.style.display = "block";
   }
 }
+
+var input = document.getElementById("msg", "name");
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("button").click();
+    }
+});
---
+++ resources/js/sweetalert.js
@@ -0,0 +1,10 @@
+function popUp(){
+  swal({
+    title: "Message sent!",
+    text: "Thank you",
+    icon: "success",
+    button: "close",
+    iconColor: "blue",
+    closeOnConfirm: false
+  });
+}
